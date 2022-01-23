b3b7d3bb4007b0fec0d2a1f3e5c4ae3658af23f8
AECIsoz <anna.isoz@outlook.com>
2022-01-23 11:44:46 +0100

    add animation to text, adjust for smaller screen

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
-$('#new-year').on('mousemove', () => {
-    $('#new-year-text').toggle();
-    })
+    $('#new-year').on('mousemove', () => {
+        $('#new-year-text').show();
+    }),
+    $('#sportlov').on('mousemove', () => {
+        $('#sportlov-text').show();
+    }),
+    $('#easter').on('mousemove', () => {
+        $('#easter-text').show();
+    }),
+    $('#matrundan').on('mousemove', () => {
+        $('#matrundan-text').show();
+    }),
+    $('#osterlen-lyser').on('mousemove', () => {
+        $('#osterlen-lyser-text').show();
+    }),
+    $('#kiviksmarknad').on('mousemove', () => {
+        $('#kiviksmarknad-text').show();
+    }),
+    $('#new-year').on('mousemove', () => {
+        $('#new-year-text').show();
+    }),
+    $('#appelveckor').on('mousemove', () => {
+        $('#appelveckor-text').show();
+    }),
+    $('#glada-barn').on('mousemove', () => {
+        $('#glada-barn-text').show();
+    }),
+    $('#kivikskortet').on('mousemove', () => {
+        $('#kivikskortet-text').show();
+    }),
+    $('#skordetid').on('mousemove', () => {
+        $('#skordetid-text').show();
+    })       
 })
